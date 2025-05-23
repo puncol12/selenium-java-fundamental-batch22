e212cd7768207ed514b81977609c3452e87b694d
[yusuf nur rizqi] <yn.yusufnur@gmail.com>
2025-05-23 22:19:20 +0700

    Fix Praktikum dari mas Yanwar (FixQuizDragAndDropTest)

---
+++ src/test/java/com/juaracoding/test/FixQuizDragAndDropTest.java
@@ -0,0 +1,68 @@
+package com.juaracoding.test;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+public class FixQuizDragAndDropTest extends BaseTest {
+    private void dragAndDrop(String idDrag, String idDrop) {
+        WebElement draggable = driver.findElement(By.id(idDrag));
+        WebElement drop = driver.findElement(By.id(idDrop));
+        Actions builder = new Actions(driver);
+
+        Action dragger = builder.clickAndHold(draggable).pause(Duration.ofMillis(500))
+                .moveToElement(drop, 0, 2).pause(Duration.ofMillis(500)).release()
+                .pause(Duration.ofMillis(500)).build();
+        dragger.perform();
+    }
+
+    private void dragAndDropReverse(String idDrag, String idDrop) {
+        WebElement drop = driver.findElement(By.id(idDrop));
+        WebElement draggable = driver.findElement(By.id(idDrag));
+        Actions builder = new Actions(driver);
+
+        Action dragger = builder.clickAndHold(draggable).pause(Duration.ofMillis(500))
+                .moveToElement(drop, 0, 2).pause(Duration.ofMillis(500)).release()
+                .pause(Duration.ofMillis(500)).build();
+        dragger.perform();
+    }
+
+    @Test
+    public void praktikumMinggu02Test01() throws InterruptedException {
+        // box1 (oslo) - box101 (norwey)
+        // box2 (stockholm) - box102 (sweden)
+
+        String[][] keyElements = {{"box1", "box101"}, {"box2", "box102"}, {"box3", "box103"},
+                {"box4", "box104"}, {"box5", "box105"}, {"box6", "box106"}, {"box7", "box107"},};
+
+        // String[][] returnToOrigin = {
+        // {"box1", "dropContent"},
+        // {"box2", "dropContent"},
+        // {"box3", "dropContent"},
+        // {"box4", "dropContent"},
+        // {"box5", "dropContent"},
+        // {"box6", "dropContent"},
+        // {"box7", "dropContent"},
+        // };
+
+        String[][] returnToOrigin = {{"box4", "dropContent"}, {"box5", "dropContent"},
+                {"box3", "dropContent"}, {"box1", "dropContent"}, {"box6", "dropContent"},
+                {"box7", "dropContent"}, {"box2", "dropContent"},};
+
+        for (int row = 0; row < keyElements.length; row++) {
+            dragAndDrop(keyElements[row][0], keyElements[row][1]);
+            Thread.sleep(500);
+        }
+
+        for (int row = 0; row < returnToOrigin.length; row++) {
+            dragAndDropReverse(returnToOrigin[row][0], returnToOrigin[row][1]);
+            Thread.sleep(500);
+        }
+
+    }
+
+
+}
--- src/test/java/com/juaracoding/test/QuizDragDropTest.java
+++ src/test/java/com/juaracoding/test/QuizDragDropTest.java
@@ -21,9 +21,13 @@ public class QuizDragDropTest extends BaseTest {
         WebElement drop = driver.findElement(By.id("box101"));
         Actions builder = new Actions(driver);
 
-        Action dragger = builder.clickAndHold(draggable).pause(Duration.ofSeconds(2))
-                .moveToElement(drop, 0, 2).pause(Duration.ofSeconds(2)).release()
-                .pause(Duration.ofSeconds(3)).build();
+        Action dragger = builder.clickAndHold(draggable)
+        .pause(Duration.ofSeconds(2))
+        .moveToElement(drop, 0, 2)
+        .pause(Duration.ofSeconds(2))
+        .release()
+        .pause(Duration.ofSeconds(3))
+        .build();
         dragger.perform();
 
         Thread.sleep(1000);
@@ -39,9 +43,13 @@ public class QuizDragDropTest extends BaseTest {
         WebElement drop = driver.findElement(By.id(idDrop));
         Actions builder = new Actions(driver);
 
-        Action dragger = builder.clickAndHold(draggable).pause(Duration.ofSeconds(2))
-                .moveToElement(drop, 0, 2).pause(Duration.ofSeconds(2)).release()
-                .pause(Duration.ofSeconds(3)).build();
+        Action dragger = builder.clickAndHold(draggable)
+        .pause(Duration.ofSeconds(2))
+        .moveToElement(drop, 0, 2)
+        .pause(Duration.ofSeconds(2))
+        .release()
+        .pause(Duration.ofSeconds(3))
+        .build();
         dragger.perform();
     }
 
@@ -50,9 +58,15 @@ public class QuizDragDropTest extends BaseTest {
         // box1 (oslo) - box101 (norwey)
         // box2 (stockholm) - box102 (sweden)
 
-        String[][] keyElements = {{"box1", "box101"}, {"box2", "box102"}, {"box3", "box103"},
-                {"box4", "box104"}, {"box5", "box105"}, {"box6", "box106"},
-            {"box7", "box107"},};
+        String[][] keyElements = {
+            {"box1", "box101"}, 
+            {"box2", "box102"}, 
+            {"box3", "box103"},
+            {"box4", "box104"}, 
+            {"box5", "box105"}, 
+            {"box6", "box106"},
+            {"box7", "box107"},
+        };
 
         for (int row = 0; row < keyElements.length; row++) {
             dragAndDrop(keyElements[row][0], keyElements[row][1]);
